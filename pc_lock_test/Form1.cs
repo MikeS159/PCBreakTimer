09429f49aefff4c9c5ec8063f734f6c574289a52
Mike <madmike159@hotmail.co.uk>
2016-02-04 18:14:26 +0000

    Added source code

---
+++ pc_lock_test/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Timers;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace PCBreakTimer
+{
+    public partial class MainProgramForm : Form
+    {        
+        private static SessionSwitchEventHandler sseh;
+        SettingsForm userSettingsForm = new SettingsForm();
+        Stopwatch homeStopWatch = new Stopwatch();
+        Stopwatch awayStopwatch = new Stopwatch();
+        Stopwatch lastBreakStopwatch = new Stopwatch();
+        Stopwatch initialStopwatch = new Stopwatch();
+        TimeSpan lastBreakTimeSpan = new TimeSpan();
+        TimeSpan homeTimeSpan = new TimeSpan();
+        TimeSpan awayTimeSpan = new TimeSpan();
+        TimeSpan homeUpdateTimeSpan = new TimeSpan();
+        TimeSpan awayUpdateTimeSpan = new TimeSpan();
+        TimeSpan totalTime = new TimeSpan();
+        NotifyIcon sysTrayIcon = new NotifyIcon();
+        TimeSpan defaultTime = Properties.Settings.Default.DefaultBreakTime;
+        TimeSpan maxTime = Properties.Settings.Default.DefaultBreakTime;
+        TimeSpan addTime = Properties.Settings.Default.AddTime;
+        TimeSpan workingDay = Properties.Settings.Default.WorkingDay;
+        TimeSpan lunchTime = Properties.Settings.Default.LunchTime;
+
+        bool firstEvent = true;
+        bool popUpWarning = Properties.Settings.Default.PopUpWarning;
+        bool startMinimized = Properties.Settings.Default.StartMinimized;
+        int currentForm = 1;
+        int windowXPos = Properties.Settings.Default.WindowXPos;
+        int windowYPos = Properties.Settings.Default.WindowYPos;
+        string timeFormat = "h'h 'm'm 's's'";
+
+
+        public MainProgramForm()
+        {
+            InitializeComponent();
+            #if DEBUG
+                testLockBtn.Enabled = true;
+                testLockBtn.Visible = true;
+                testUnlockBtn.Enabled = true;
+                testUnlockBtn.Visible = true;
+            #endif
+        }
+
+        /// <summary>
+        /// Called when the form is loaded, sets up user options
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Left = windowXPos;
+            this.Top = windowYPos;
+            sseh = new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            SystemEvents.SessionSwitch += sseh;
+            richTextBox1.AppendText(DateTime.Now.ToString() + "\n");
+            sysTrayIcon.MouseDoubleClick += new MouseEventHandler(sysTrayIcon_MouseDoubleClick);
+            sysTrayIcon.BalloonTipText = "Timers Running";
+            this.sysTrayIcon.Icon = this.Icon;
+            sysTrayIcon.Visible = true;
+            sysTrayIcon.ShowBalloonTip(500);
+            if(startMinimized)
+            {
+                minimizeWindow();
+            }
+            Start();
+        }
+
+        /// <summary>
+        /// Called when the session switch event is triggered
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            string s = DateTime.Now.ToString();
+            s = s + " " + e.Reason.ToString();
+            richTextBox1.AppendText(s + "\n");
+            if (e.Reason == SessionSwitchReason.SessionLogon)
+            {
+                Start();
+            }
+            if (e.Reason == SessionSwitchReason.SessionLogoff)
+            {
+                Stop();
+            }
+            if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                Unlock();
+            }
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                Lock();
+            }
+            if (e.Reason == SessionSwitchReason.ConsoleConnect)
+            {
+                Unlock();
+            }
+            if (e.Reason == SessionSwitchReason.ConsoleDisconnect)
+            {
+                Lock();
+            }
+        }
+
+        private void Start()
+        {
+            homeStopWatch.Start();
+            initialStopwatch.Start();
+            lastBreakStopwatch.Start();
+        }
+
+        private void Stop()
+        {
+            homeStopWatch.Stop();            
+            homeTimeSpan = homeTimeSpan + homeStopWatch.Elapsed;
+            homeStopWatch.Reset();
+        }
+
+        private void Lock()
+        {
+            homeStopWatch.Stop();
+            awayStopwatch.Start();
+            lastBreakStopwatch.Stop();
+            lastBreakStopwatch.Reset();
+            homeTimeSpan = homeTimeSpan + homeStopWatch.Elapsed;
+            homeStopWatch.Reset();
+            HomeTimeLabel.Text = homeTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+            maxTime = defaultTime;
+            BreakWarningLabel.Visible = false;
+            if (firstEvent) //This corrects timers if the program starts when the computer is locked
+            {
+                firstEvent = false;
+                initialStopwatch.Reset();
+            }
+        }
+
+        private void Unlock()
+        {
+            if (!firstEvent)
+            {
+                awayStopwatch.Stop();
+                homeStopWatch.Start();
+                awayTimeSpan = awayTimeSpan + awayStopwatch.Elapsed;
+                awayStopwatch.Reset();
+                AwayTimeLabel.Text = awayTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+                lastBreakStopwatch.Start();
+            }
+            else
+            {
+                awayStopwatch.Stop();
+                initialStopwatch.Stop();
+                homeStopWatch.Restart();
+                awayTimeSpan = awayTimeSpan + initialStopwatch.Elapsed;
+                awayStopwatch.Reset();
+                initialStopwatch.Reset();
+                HomeTimeLabel.Text = homeTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+                AwayTimeLabel.Text = awayTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+                lastBreakStopwatch.Restart();
+                firstEvent = false;
+            }
+        }
+
+        /// <summary>
+        /// Used to update forms
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            homeUpdateTimeSpan = homeTimeSpan + homeStopWatch.Elapsed;
+            HomeTimeLabel.Text = homeUpdateTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+            awayUpdateTimeSpan = awayTimeSpan + awayStopwatch.Elapsed;
+            AwayTimeLabel.Text = awayUpdateTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+            lastBreakTimeSpan = lastBreakStopwatch.Elapsed;
+            LastBreakLabel.Text = lastBreakTimeSpan.ToString(timeFormat, CultureInfo.InvariantCulture);
+            totalTime = homeUpdateTimeSpan + awayUpdateTimeSpan;
+            TotalTimeLabel.Text = totalTime.ToString(timeFormat, CultureInfo.InvariantCulture);
+            if (lastBreakTimeSpan > maxTime)
+            {                
+                this.WindowState = FormWindowState.Normal;
+                this.Activate();
+                maxTime = maxTime.Add(addTime);
+                BreakWarningLabel.Visible = true;
+                if (popUpWarning)
+                {
+                    MessageBox.Show("Take a Break!", "Coffee Time", MessageBoxButtons.OK, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+                }
+            }
+            if (totalTime > (workingDay + lunchTime))
+            {
+                label4.ForeColor = Color.Red;
+                TotalTimeLabel.ForeColor = Color.Red;
+            }
+            else
+            {
+                label4.ForeColor = Color.Black;
+                TotalTimeLabel.ForeColor = Color.Black;
+            }
+            double percentageAtDesk = 0;
+            percentageAtDesk = (100 - ((awayTimeSpan.TotalMilliseconds / totalTime.TotalMilliseconds) * 100));
+            percentageAtDesk = Math.Round(percentageAtDesk, 2);
+            PercentageLabel.Text = percentageAtDesk.ToString(CultureInfo.InvariantCulture) + " %";
+        }
+
+        private void sysTrayIcon_MouseDoubleClick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            showCurrentForm();          
+        }
+
+        private void showCurrentForm()
+        {
+            switch (currentForm)
+            {
+                case 1:
+                    this.Show();
+                    this.Activate();
+                    this.WindowState = FormWindowState.Normal;
+                    this.Left = windowXPos;
+                    this.Top = windowYPos;
+                    this.Height = 255;
+                    this.Width = 493;
+                    break;
+                case 2:
+                    userSettingsForm.Left = windowXPos;
+                    userSettingsForm.Top = windowYPos;
+                    userSettingsForm.Show();
+                    userSettingsForm.Activate();
+                    userSettingsForm.WindowState = FormWindowState.Normal;
+                    break;
+                default:
+                    this.Show();
+                    this.Activate();
+                    this.WindowState = FormWindowState.Normal;
+                    this.Left = windowXPos;
+                    this.Top = windowYPos;
+                    this.Height = 255;
+                    this.Width = 493;
+                    break;
+            }
+        }
+
+        private void minimizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            minimizeWindow();
+        }
+
+        private void minimizeWindow()
+        {
+            windowXPos = this.Left;
+            windowYPos = this.Top;
+            timer1.Enabled = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            DialogResult closeResult = MessageBox.Show("Are you sure you want to close?", "Close Program", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2, MessageBoxOptions.RightAlign);
+            if (closeResult == DialogResult.Yes)
+            {
+                sysTrayIcon.Visible = false;
+                sysTrayIcon.Dispose();
+                this.Close();
+            }
+            else if (closeResult == DialogResult.No)
+            {
+
+            }            
+        }
+
+        private void readMeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Coming in future version... probably");
+        }
+
+        private void versionHistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Version_History.returnString());
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Assembly assem = Assembly.GetEntryAssembly();
+            string programName = FileVersionInfo.GetVersionInfo(assem.Location).ProductName;
+            string companyName = FileVersionInfo.GetVersionInfo(assem.Location).CompanyName;
+            string copyright = FileVersionInfo.GetVersionInfo(assem.Location).LegalCopyright;
+            string comments = FileVersionInfo.GetVersionInfo(assem.Location).Comments;
+            string assemVer = FileVersionInfo.GetVersionInfo(assem.Location).FileVersion;
+            string infoVer = FileVersionInfo.GetVersionInfo(assem.Location). ProductVersion;
+            string s =
+            programName + " by " +
+            companyName + "\n" +
+            copyright + "\n\n" +
+            "Version: " + infoVer + " (" + assemVer + ")";
+            MessageBox.Show(s);
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            currentForm = 2;
+            userSettingsForm.ShowDialog();
+            updateUserSettings();
+            currentForm = 1;
+            showCurrentForm();
+            //Code to get settings goes here
+        }
+
+        private void updateUserSettings()
+        {
+            lunchTime = Properties.Settings.Default.LunchTime;
+            workingDay = Properties.Settings.Default.WorkingDay;
+            addTime = Properties.Settings.Default.AddTime;
+            defaultTime = Properties.Settings.Default.DefaultBreakTime;
+            //maxTime = defaultTime;
+            windowXPos = Properties.Settings.Default.WindowXPos;
+            windowYPos = Properties.Settings.Default.WindowYPos;
+            popUpWarning = Properties.Settings.Default.PopUpWarning;
+        }
+
+        private void testLockBtn_Click(object sender, EventArgs e)
+        {
+            Lock();
+            richTextBox1.AppendText(DateTime.Now.ToString() + " Test Lock\n");
+        }
+
+        private void testUnlockBtn_Click(object sender, EventArgs e)
+        {
+            Unlock();
+            richTextBox1.AppendText(DateTime.Now.ToString() + " Test Unlock\n");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.ScrollToCaret();
+        }
+    }
+}
---
+++ pc_lock_test/Form2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PCBreakTimer
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            setFormToUserSettings();
+        }
+
+        private void setFormToUserSettings()
+        {
+            LunchTimeTextBox.Text = Properties.Settings.Default.LunchTime.ToString();
+            WorkDayTextBox.Text = Properties.Settings.Default.WorkingDay.ToString();
+            AddTimeTextBox.Text = Properties.Settings.Default.AddTime.ToString();
+            MaxTimeTextBox.Text = Properties.Settings.Default.DefaultBreakTime.ToString();
+            WinXPosTextBox.Text = Properties.Settings.Default.WindowXPos.ToString();
+            WinYPosTextBox.Text = Properties.Settings.Default.WindowYPos.ToString();
+            PopupMessageCheckBox.Checked = Properties.Settings.Default.PopUpWarning;
+            StartMinimizedCheckBox.Checked = Properties.Settings.Default.StartMinimized;
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            setFormToUserSettings();
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            TimeSpan tempSpan = new TimeSpan();
+            
+            if (TimeSpan.TryParse(LunchTimeTextBox.Text, out tempSpan))
+            {
+                Properties.Settings.Default.LunchTime = tempSpan;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Please enter time in a valid format");
+            }
+            
+            if (TimeSpan.TryParse(WorkDayTextBox.Text, out tempSpan))
+            {
+                Properties.Settings.Default.WorkingDay = tempSpan;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Please enter time in a valid format");
+            }
+            if (TimeSpan.TryParse(AddTimeTextBox.Text, out tempSpan))
+            {
+                Properties.Settings.Default.AddTime = tempSpan;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Please enter time in a valid format");
+            }
+            if (TimeSpan.TryParse(MaxTimeTextBox.Text, out tempSpan))
+            {
+                Properties.Settings.Default.DefaultBreakTime = tempSpan;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Please enter time in a valid format");
+            }
+            int tempXPos = 0;
+            int tempYPos = 0;
+            if (Int32.TryParse(WinXPosTextBox.Text, out tempXPos))
+            {
+                Properties.Settings.Default.WindowXPos = tempXPos;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer");
+            }
+            if (Int32.TryParse(WinYPosTextBox.Text, out tempYPos))
+            {
+                Properties.Settings.Default.WindowYPos = tempYPos;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer");
+            }
+            Properties.Settings.Default.PopUpWarning = PopupMessageCheckBox.Checked;
+            Properties.Settings.Default.StartMinimized = StartMinimizedCheckBox.Checked;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
